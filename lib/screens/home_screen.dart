5e211a89a7b63a9fa2e04292f03f56d8301a77a8
czmatejt <tajovsky.matej@gmail.com>
2025-05-05 20:06:27 +0200

    show urgent message on home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,6 +5,7 @@ import 'package:ak_kurim_app/l10n/app_localizations.dart';
 import '../providers/meet_providers.dart';
 import '../providers/training_providers.dart';
 import './attendance_tresults/trainings_screen.dart';
+import '../providers/remote_config_provider.dart';
 
 class HomeScreen extends HookConsumerWidget {
   const HomeScreen({super.key});
@@ -13,13 +14,42 @@ class HomeScreen extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final trainings = ref.watch(trainingsPProvider);
     final meets = ref.watch(meetProvidersPProvider);
+    final remoteConfig = ref.watch(remoteConfigProvider);
     return trainings.when(
       data: (trainingsList) => meets.when(
         data: (meetsList) => Padding(
-          padding: const EdgeInsets.all(8.0),
+          padding: const EdgeInsets.all(0.0),
           child: SingleChildScrollView(
             child: Column(
               children: [
+                remoteConfig.when(
+                  data: (data) {
+                    return data.urgentMessage != null &&
+                            data.urgentMessage!.isNotEmpty
+                        ? Column(
+                            children: [
+                              Container(
+                                padding: const EdgeInsets.all(8),
+                                decoration: BoxDecoration(
+                                  color: Colors.red,
+                                  borderRadius: BorderRadius.circular(8),
+                                ),
+                                child: Text(
+                                  data.urgentMessage!,
+                                  style: const TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                              const SizedBox(height: 10),
+                            ],
+                          )
+                        : const SizedBox();
+                  },
+                  error: (error, stackTrace) => const SizedBox(),
+                  loading: () => const SizedBox(),
+                ),
                 Text(AppLocalizations.of(context)!.nextTraining,
                     style: Theme.of(context).textTheme.headlineSmall),
                 trainingsList.isNotEmpty
